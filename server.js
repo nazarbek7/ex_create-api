af05aa74aafa2468605c693d621451467643d486
Nazarbek Erkinov <nazarbek.erkinov@gmail.com>
2021-09-20 22:22:36 +0600

    create draft & get done

--- server.js
+++ server.js
@@ -11,7 +11,36 @@ app.get('/', function(req, res) {
 // CSS Prefixed
 app.use(express.static(__dirname + '/'));
 
-// Endpoint to Get a list of users
+// Endpoint to Create a value
+app.post('/createValue', function(req, res){
+    fs.writeFile(__dirname + "/db/data.json", 'utf8', function(err, data){
+        console.log(data);
+        res.end(data); // you can also use res.send()
+    });
+})
+
+let value = {
+    "value7": {
+        "id":7,
+        "value": "77",
+        "fix_value": "2",
+        "result": "144"
+    },
+}
+
+//The addUser endpoint
+app.post('/createValue', function(req, res){
+    //Step 2: read existing users
+    fs.readFile(__dirname + "/db/users.json", 'utf8', function(err, data){
+        data = JSON.parse(data);
+        //Step 3: append user variable to list
+        data["value7"] = value["value7"];
+        console.log(data);
+        res.end(JSON.stringify(data));
+    });
+})
+
+// Endpoint to Get a list of values
 app.get('/getValue', function(req, res){
     fs.readFile(__dirname + "/db/" + "data.json", 'utf8', function(err, data){
         console.log(data);
